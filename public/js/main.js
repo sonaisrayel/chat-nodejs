e2d423d6584004d262609c565329ed1d56722a4d
sonaisrayel <israyelyan.sona@gmail.com>
2020-12-26 00:55:07 +0300

    set up client input clear, scroll

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,15 @@
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
 
 socket.on('message', message => {
     outputMessage(message);
+
+    //Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+
 
 
 });
@@ -16,6 +21,11 @@ chatForm.addEventListener('submit', (e) => {
     const msg = e.target.elements.msg.value;
     //emmiting message to the server
     socket.emit('chatMessage', msg);
+
+    //Clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+
 
 });
 
