2ca4436f20f2664467fc4e0730333997a6d0c3f1
sonaisrayel <israyelyan.sona@gmail.com>
2020-12-25 21:40:33 +0300

    add socket io connection in backend

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+
+const path = require('path');
+const http = require('http');
 const express = require('express');
 const app = express();
+const socketio = require('socket.io')
+const server = http.createServer(app);
 
+const io = socketio(server);
 const PORT = 3000 | process.env.PORT;
 
+//static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+// Run when clinet conneects
+
+io.on('connection', socket => {
+    console.log('new socket coonnection')
+})
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
